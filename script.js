a727be92056f279e4f57f07e2e8783882f53a873
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2021-02-18 15:47:20 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
 const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August","September",
+"October", "November", "December"];
 
-const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August","September", "October", "November", "December"];
+const M = new Date();
 
-const m = new Date();
-const d = new Day();
-document.write(dayNames[d.getDay()] + monthNames[m.getMonth()]);
+document.write(dayNames[M.getDay()] + ',' + ' ' + monthNames[M.getMonth()]);
 
 var space = ' '
-
 document.write(space)
 
-var today = new Date();
+var today = new Date(); 
 var dd = String(today.getDate()).padStart(2,'0');
 var yyyy = today.getFullYear();
 
-today = '' + dd + ' ' + yyyy
+today = '' + dd + ' ' +  yyyy;
 document.write(today);
+
+var time = new Date();
+var hour = time.getHours();
+var min = time.getMinutes();
+var sec = time.getSeconds();
+var milli = time.getMilliseconds();
+
+var space = ' '
+document.write(space)
+
+time = hour + ':' + min + ':' + sec + ':' + milli
+document.write(time);
+
+function increment() {
+  time ++;
+  console.log(i);
+}
+setInterval(increment, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
