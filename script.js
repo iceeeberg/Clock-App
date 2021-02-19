023c25590bee9fa845b09e4f855cd646eeaddf84
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2021-02-19 17:17:17 -0600

    third commit

--- script.js
+++ script.js
@@ -4,53 +4,50 @@ const monthNames = ["January", "February", "March", "April", "May", "June", "Jul
 
 const M = new Date();
 
-document.write(dayNames[M.getDay()] + ',' + ' ' + monthNames[M.getMonth()]);
-
-var space = ' '
-document.write(space)
-
 var today = new Date(); 
-var dd = String(today.getDate()).padStart(2,'0');
+var dd = String(today.getDate()).slice(-2);
 var yyyy = today.getFullYear();
+var ordinal;
+  if (dd > 3 && dd < 21) 
+  ordinal = dd +'th';
+  switch (dd % 10) {
+    case 1: ordinal = dd + "st";
+    case 2: ordinal = dd + "nd";
+    case 3: ordinal = dd + "rd";
+    default: ordinal = dd + "th";
+};
 
-today = '' + dd + ' ' +  yyyy;
-document.write(today);
 
-var time = new Date();
-var hour = time.getHours();
-var min = time.getMinutes();
-var sec = time.getSeconds();
-var milli = time.getMilliseconds();
+const currentDate = document.getElementById('date');
+currentDate.textContent = (dayNames[M.getDay()] + ',' + ' ' + monthNames[M.getMonth()]) + ' ' + ordinal + ' ' + 
+yyyy; 
 
 var space = ' '
 document.write(space)
-
-time = hour + ':' + min + ':' + sec + ':' + milli
-document.write(time);
 
 function increment() {
-  time ++;
-  console.log(i);
+var time = new Date();
+var hour = time.getHours()
+var min = ('0' + time.getMinutes()).slice(-2);
+var sec = ('0' + time.getSeconds()).slice(-2);
+var amPM;
+
+if (hour > 12) {
+  amPM = 'PM';
+  hour = hour - 12 
+} else {
+    amPM = 'AM';
+}
+var leadingZero;
+
+if (hour < 10) {
+  leadingZero = '0' 
+ } else {
+    delete leadingZero
+  }
+
+const currentTime = document.getElementById('time');
+currentTime.textContent = leadingZero + hour + ':' + min + ':' + sec + ' ' + amPM
 }
 setInterval(increment, 1000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
